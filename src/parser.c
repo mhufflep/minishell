7b791d334bf9b1fd6c4b633175c4af5bab2dabd4
levensta <levensta@student.21-school.ru>
2021-05-10 22:09:36 +0300

    sketch of the parser architecture

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,84 @@
 #include "minishell.h"
 
-void parse_line(t_prm *prm)
+int		skip_spaces(char *str)
 {
-	(void)prm;
+	int	i;
+
+	i = 0;
+	while (str[i] == " ")
+		i++;
+	return (i);
+}
+
+void	parse_line(t_prm *prm)
+{
+	t_cmd		*command;
+	t_bd_lst	*new;
+	char		is_quote;
+	char		is_dquote;
+	int			i;
+
+	is_quote = 0;
+	is_dquote = 0;
+	i = 0;
+
+	command = malloc(sizeof(t_cmd));
+	new = bd_lstnew(command);
+	if (new == NULL)
+		exit(1);
+	bd_lstadd_back(&(prm->cmds), new);
+
+	i = skip_spaces(prm->line);
+	if (prm->line[i] == "\"")
+	{
+		is_dquote = 1;
+		i++;
+	}
+	else if (prm->line[i] == "\'")
+	{
+		is_quote = 1;
+		i++;
+	}
+	// parse
+	while (prm->line[i])
+	{
+		if (is_quote)
+		{
+			while (prm->line[i] != "\'" || prm->line[i])
+			{
+				// if (prm->line[i] == "-")
+				// 	is_option = 1;
+				// if (is_option)
+				// 	command->options = ...
+				// if (is_command)
+				// 	command->cmd = ...
+				// ...
+				i++;
+				if (prm->line[i] == "\'")
+					is_quote = 0;
+			}
+			if (is_quote)
+				exit(1); // Сlosing quotation mark is not found.
+		}
+		else if (is_dquote) // все то же самое только с двойной кавычкой
+		{
+			while (prm->line[i] != "\"" || prm->line[i])
+			{
+				//
+				i++;
+				if (prm->line[i] == "\"")
+					is_dquote = 0; 
+			}
+			if (is_quote)
+				exit(1); // Сlosing double quotation mark is not found.
+		}
+		else
+		{
+			//
+			i++;
+		}
+	}
+	//
+	new->content = command;
+	// prm->cmds_ptr = bd_lstlast(prm->cmds);
 }
