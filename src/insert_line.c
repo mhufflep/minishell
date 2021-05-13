ae02a41b0ab1b31c577c941641a0ead73b8c4511
levensta <levensta@student.21-school.ru>
2021-05-13 23:25:45 +0300

    modified insert_line(), remove_substr() [tested]

--- src/insert_line.c
+++ src/insert_line.c
@@ -1,25 +1,17 @@
 #include "libft.h"
-#include <stdio.h>
 
-void	insert_line(char *str, char *substr, int i)
+// Inserts a substring in the specified index i of str
+// Returns a pointer to the finished string
+
+char	*insert_line(char *str, char *substr, int i)
 {
 	char	*str1;
 	char	*str2;
-	(void)substr;
 
 	str1 = ft_substr(str, 0, i);
 	str2 = ft_substr(str, i, ft_strlen(str));
-
 	str1 = ft_strjoin_free(str1, substr);
-	str2 = ft_strjoin_free(str1, str2);
-	// printf("|%s|\n", str2);
-	return (str);
+	str1 = ft_strjoin_free(str1, str2);
+	free(str2);
+	return (str1);
 }
-
-int main ()
-{
-	insert_line("hello", "90", 2); // he90llo
-	return (0);
-}
-
-// gccw -L ./libft -lft -I ./libft/ insert_line.c
--- src/remove_substr.c
+++ src/remove_substr.c
@@ -1,27 +1,18 @@
 #include "libft.h"
-#include <stdio.h>
 
-char	*remove_substr(char *str, unsigned int start, unsigned int len)
+// Takes a pointer to a string str and cuts from it
+// the number of len characters, starting from the start position
+
+void	remove_substr(char **str, unsigned int start, unsigned int len)
 {
 	char	*str1;
 	char	*str2;
 
-	str1 = ft_substr(str, 0, start);
-	str2 = ft_substr(str, start + len, ft_strlen(str));
-	// printf("|%s|\n", str1);
-	// printf("|%s|\n", str2);
-	str1 = ft_strjoin_free(str1, str2);
+	if (!str || !*str)
+		return ;
+	str1 = ft_substr(*str, 0, start);
+	str2 = ft_substr(*str, start + len, ft_strlen(&(*str)[start + len]));
+	free(*str);
+	*str = ft_strjoin_free(str1, str2);
 	free(str2);
-	free(str);
-	// printf("|%s|\n", str1);
-	return (str1);
 }
-
-int main ()
-{
-	char *s = ft_strdup("hello");
-	s = remove_substr(s, 0, 0);
-	return (0);
-}
-
-// gccw -L ./libft -lft -I ./libft/ remove_substr.c
