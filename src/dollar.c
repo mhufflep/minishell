0039dd60affa4d8d67d510063bcab169dd05c7dd
levensta <levensta@student.21-school.ru>
2021-06-14 17:27:26 +0300

    hotfix: leaks in parse_dollar

--- src/dollar.c
+++ src/dollar.c
@@ -5,6 +5,7 @@ static	void	insert_value(char **s, int i)
 	int		start;
 	int		len;
 	t_env	*var;
+	char	*key;
 
 	len = 0;
 	start = 0;
@@ -15,7 +16,9 @@ static	void	insert_value(char **s, int i)
 		len++;
 		i++;
 	}
-	var = env_get_local(ft_substr(*s, start, len));
+	key = ft_substr(*s, start, len);
+	var = env_get_local(key);
+	free(key);
 	if (var)
 		replace_by(s, start, len, var->val, free);
 	else
@@ -25,7 +28,7 @@ static	void	insert_value(char **s, int i)
 void			parse_dollar(char **s, int code)
 {
 	int		i;
-	char	*exit_;
+	char	*exit_num;
 
 	i = 0;
 	while (*s && (*s)[i])
@@ -42,9 +45,9 @@ void			parse_dollar(char **s, int code)
 		}
 		else if ((*s)[i] == '$' && !is_slash(*s, i - 1) && (*s)[i + 1] == '?')
 		{
-			replace_by(s, i, 2, exit_ = ft_itoa(code), free);
-			i += ft_strlen(exit_);
-			free(exit_);
+			replace_by(s, i, 2, exit_num = ft_itoa(code), free);
+			i += ft_strlen(exit_num);
+			free(exit_num);
 		}
 		else
 			i++;
